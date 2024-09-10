f1e94769db7c7902cae6c6835bcfc1f3c8a8a739
Karthika2706 <karthikavengadesh477@gmail.com>
2024-09-10 11:11:48 +0530

    Add files via upload

---
+++ microblogging/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+let posts = [];
+let users = {}; 
+
+app.post('/post', (req, res) => {
+    const content = req.body.content;
+    const post = { content, timestamp: new Date() };
+    posts.push(post);
+    res.status(201).send('Post created');
+});
+app.get('/feed', (req, res) => {
+    res.json(posts);
+});
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+    console.log(`Microblogging app listening at http://localhost:${port}`);
+});
